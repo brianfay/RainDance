68048614bbe861354b1eb9f72893c5156d583b8e
Brian Fay <ovaltinevortex@gmail.com>
2013-02-07 23:59:55 -0500

    Update GameSounds.js

--- GameSounds.js
+++ GameSounds.js
@@ -2,7 +2,7 @@ var context = new webkitAudioContext();
 var bound = false;
 
 function bindJavascript(){
-  var pjs = Processing.getInstanceById('RainDanceSketch');
+	var pjs = Processing.getInstanceById('RainDanceSketch');
 	if(pjs!=null){
 		pjs.bindJavascript(this);
 		bound = true;
@@ -14,35 +14,40 @@ bindJavascript();
 var soundSource, soundBuffer;
 
 function playSound(soundSource, ampEnv){
-	soundSource.noteOn(context.currentTime);
-	soundSource.connect(ampEnv);
-	ampEnv.gain.cancelScheduledValues(0);
-	ampEnv.gain.setTargetAtTime(0.3, context.currentTime, 0.001);
-	ampEnv.gain.setTargetAtTime(0, context.currentTime + 0.25, 0.25);
-	ampEnv.connect(context.destination);
-	stopSound(soundSource,2);
+
 }
 
 function stopSound(soundSource, time){
 	soundSource.noteOff(context.currentTime + time);
+	soundSource.stop(context.currentTime + time);
 }
 
-function playSynth(){
+function playChirp(){
 	var cosineHarmonics = new Float32Array([0,0,0]);
-	var sineHarmonics = new Float32Array([1,0.3,0.2]);
+	var sineHarmonics = new Float32Array([1,0.5,1]);
 	var myWaveTable = context.createWaveTable(cosineHarmonics,sineHarmonics);
 	var ampEnv = context.createGainNode();
 	ampEnv.gain.value = 0;
 	var myOsc = context.createOscillator();
 	myOsc.setWaveTable(myWaveTable);
-	var noteChoice = parseInt((Math.random()*5))
-	var theNote = (noteChoice + 1) * 440;
+	var fundamentalFreq = 440;
+	var pentatonicScale = [0, 2, 4, 7, 9, 12, 14, 16, 19, 21];	
+	var scaleIndex = Math.floor(Math.random()*pentatonicScale.length);
+	var actualNote = mtof(fundamentalFreq, pentatonicScale[scaleIndex]);
 	
-	myOsc.frequency.value = theNote;
-	playSound(myOsc,ampEnv);
+	myOsc.connect(ampEnv);
+	myOsc.frequency.value = actualNote * 0.5;
+	myOsc.noteOn(context.currentTime);
+	myOsc.frequency.setTargetAtTime(actualNote, context.currentTime, 0.05);	
+	ampEnv.gain.cancelScheduledValues(0);
+	ampEnv.gain.setTargetAtTime(0.1, context.currentTime, 0.001);
+	ampEnv.gain.setTargetAtTime(0, context.currentTime + 0.25, 0.25);
+	ampEnv.connect(context.destination);
+	stopSound(myOsc,2);
 }
 
-window.onload = function(){
-	var playButton = document.getElementById("playButton");
-	playButton.onclick = function(){playSynth()};
-};
+function mtof(freq, shift){
+	var adjFreq = freq*(Math.pow(2,(shift/12)));
+	return adjFreq;
+}
+
